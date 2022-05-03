16ae4e9de0da93243f6d281855b3a640610c8058
Ramsey <ramseyv2@illinois.edu>
2022-05-03 13:03:05 -0500

    import

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 # Problems with imports? Could try `export PYTHONPATH=$PYTHONPATH:`pwd`` from root project dir...
 import os
 import sys
+import ml_pipeline
 sys.path.insert(0, os.path.abspath('..'))  # Source code dir relative to this file
 
 # -- Project information -----------------------------------------------------
